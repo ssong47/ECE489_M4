69407ec806391a424c69d1e13b62d1ace3367df6
ningyuandu <63272603+ningyuandu@users.noreply.github.com>
2020-04-20 16:40:09 -0500

    Add files via upload

--- region_plot.m
+++ region_plot.m
@@ -15,10 +15,10 @@ phi_2H = -tau_H/N_H - tau_Imax <= 0 ;
 phi_3H = tau_H/N_H + tau_stall*N_H*omega_H/omega_NL - tau_stall <= 0;
 phi_4H = -tau_H/N_H - tau_stall*N_H*omega_H/omega_NL - tau_stall <= 0;
 
-phi_1K = tau_K/N_K - tau_Imax <= 0 ; 
-phi_2K = -tau_K/N_K - tau_Imax <= 0 ;
-phi_3K = tau_K/N_K + tau_stall*N_K*omega_K/omega_NL - tau_stall <= 0;
-phi_4K = -tau_K/N_K - tau_stall*N_K*omega_K/omega_NL - tau_stall <= 0;
+phi_1K = tau_H/N_K - tau_Imax <= 0 ; 
+phi_2K = -tau_H/N_K - tau_Imax <= 0 ;
+phi_3K = tau_H/N_K + tau_stall*N_K*omega_H/omega_NL - tau_stall <= 0;
+phi_4K = -tau_H/N_K - tau_stall*N_K*omega_H/omega_NL - tau_stall <= 0;
 
 both_H = phi_1H & phi_2H & phi_3H & phi_4H ;    
 both_K = phi_1K & phi_2K & phi_3K & phi_4K ;%# Intersection of both inequations
@@ -30,6 +30,7 @@ contourf(omega_H,tau_H,c(1) * both_H, [c(1), c(1)], 'red')
 contourf(omega_H,tau_H,c(2) * both_K, [c(2), c(2)], 'green')  %# Fill area for second inequation
  
 contourf(omega_H,tau_H,c(3) * both, [c(3), c(3)], 'blue')
+colormap(white)
 xlabel('\omega_H (rad/s)')
 ylabel('\tau_H (N.m)')
 legend('Region of Hip', 'Region of Knee','Overlap')
